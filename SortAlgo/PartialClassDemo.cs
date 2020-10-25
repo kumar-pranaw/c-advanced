98c507447c187b895e43b03170c13056badbf3e5
Pranav <sanupranav@outlook.com>
2020-10-26 02:05:35 +0530

    Added design pattern code

---
+++ SortAlgo/PartialClassDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortAlgo
+{
+    public static partial class PartialClass
+    {
+        public static string EmpFirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public static string EmpLastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public static string MyName()
+        {
+            return _firstName + " " + _lastName;
+        }
+    }
+}
